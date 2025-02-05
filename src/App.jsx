e722216be2e90d7b14f6cdf9400d9bfbf39cd320
Dinesh <160803395+Dinesh0474@users.noreply.github.com>
2025-02-05 15:31:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,3 @@
-// import './App.css'
-// import Home from './Home'
-// import Header from './Header'
-// import { BlogProvider } from './BlogProvider'
-
-
-// function App() {
-  
-
-//   return (
-//     <>
-//     <div className="w-full min-h-screen  bg-[#1a1a1a] flex flex-col">
-//       <BlogProvider>
-//         <Header/>
-//         <Home/>
-//       </BlogProvider>
-    
-//     </div>
-//     </>
-//   )
-// }
-
-// export default App
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import NewBlogPage from './NewBlogPage';
